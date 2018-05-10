560c44f03ba437be58c3c89cf4b247cbfed88295
Iggy Grey <github@pmphillips.ca>
2018-05-10 13:07:15 -0700

    Fixed for Python3. Adjusted googletrends to accept multiple terms (although that feature still hasn't been added to TrendViz as a whole)

--- trendviz.py
+++ trendviz.py
@@ -1,10 +1,13 @@
 import sys
-sys.path.insert(0, 'scripts')
 import json
+import pandas as pd
 
+sys.path.insert(0, 'scripts')
 import googletrends
-import plotly_twoline
 import coinmarketcap
+import cryptocompare
+import fudhud
+import plotly_fourline
 
 
 def getSymbol(coinname):
@@ -19,30 +22,79 @@ try:
 
     coin = sys.argv[2]
     term = coin.replace('-', ' ')
+
 except Exception as e:
-    print e
     raise
 
 try:
     sym = getSymbol(coin)
-    print 'Retrieved Symbol: ' + sym
+    print('Retrieved Symbol: ' + sym)
 except:
     #Download symbol data from coinmarketcap, current limit is set to top 200 coins
     coinlimit = 200
 
-    print 'Downloading symbols for top ' + str(coinlimit) + ' coins...'
+    print('Downloading symbols for top ' + str(coinlimit) + ' coins...')
     coinmarketcap.downloadSymbols(coinlimit, 'symbols.json')
     try:
         sym = getSymbol(coin)
-        print 'Retrieved Symbol: ' + sym
+        print('Retrieved Symbol: ' + sym)
     except Exception as e:
-        print e
+        print(e)
 
 try:
-    print 'Gathering data from Google Trends and CryptoCompare...'
-    googletrends.get_data(term, sym, timescale)
-    print 'Complete'
-    print 'Creating visualization...'
-    plotly_twoline.plotGraph('csv/' + sym.lower() + '.csv', 'Date', term.title() + ' Trends', 'Historical Price (USD)', term.title() + ' Trends vs Price', term.title() + ' Trends', 'Historical Price (USD)')
+    print('Gathering data from Google Trends and CryptoCompare...')
+    csvfile = googletrends.get_data([term], timescale)
+    df = pd.read_csv(csvfile, delimiter=',')
+    timelist = list(df['Date'])
+    pricelist = cryptocompare.lookupPrice(timelist, sym, 'USD', True)
+    #df['Historical Prices (USD)'] = pd.Series(pricelist, index=df.index)
+    #df = df.assign('Historical Prices (USD)'=p.Series(pricelist).values)
+    df['Historical Price (USD)'] = pricelist
+    df.to_csv(csvfile)
+
+    data = {
+        'title' :  term.title() + ' Trends vs Price',
+        'data' : []
+    }
+    if timescale == 'week':
+        mentions = fudhud.getMentions(['$' + sym.lower()])
+        data['data'].append({
+            'title' : 'Twitter',
+            'xlabel' : 'Date',
+            'x' : mentions['twitter-date'],
+            'ylabel' : 'Mentions',
+            'y' : mentions['twitter-$' + sym.lower()],
+            'yaxis' : 'y3'
+        })
+        data['data'].append({
+            'title' : 'Reddit',
+            'xlabel' : 'Date',
+            'x' : mentions['reddit-date'],
+            'ylabel' : 'Mentions',
+            'y' : mentions['reddit-$' + sym.lower()],
+            'yaxis' : 'y3'
+        })
+
+    data['data'].append({
+        'title' : 'Price',
+        'xlabel' : 'Date',
+        'x' : timelist,
+        'ylabel' : 'Historical Price (USD)',
+        'y' : pricelist,
+        'yaxis' : 'y1'
+    })
+    data['data'].append({
+        'title' : 'Google Trends',
+        'xlabel' : 'Date',
+        'x' : timelist,
+        'ylabel' : 'Google Trends',
+        'y' : list(df[term.title() + ' Trends']),
+        'yaxis' : 'y2'
+    })
+
+
+    print('Complete')
+    print('Creating visualization...')
+    plotly_fourline.plotGraph(data)
 except Exception as e:
-    print e
+    print(e)
